9086b87b2c70eaf71a8f655141d7f8c80da707f7
Hyejic <hyeji0314@gmail.com>
2021-12-17 21:39:26 +0900

    layout 수정 및 slide up down 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 window.addEventListener('load', () => {
   
   const spendSec = document.querySelector('.spend-section');
-  
-  spendSec.addEventListener('click', () => {
-    if(spendSec.classList.contains('open')){
-      spendSec.classList.remove('open')
-    }else {
-      spendSec.classList.add('open')
+  const dragBar = document.querySelector('.dragbar');
+  let targetCheck = false;
+  dragBar.addEventListener('touchstart', () => {
+    targetCheck = true;
+    // console.log('targetCheck',targetCheck)
+  })
+
+  let touchStartY = null;
+  let moveY = null;
+  function touchStart(e) {
+    const touch = e.touches[0];
+    touchStartY = touch.clientY;
+    // console.log('touchStartY',touchStartY)
+  }
+  function touchMove(e) {
+    const touch = e.touches[0];
+    let presentY = touch.clientY;
+    if(touchStartY !== null){
+      moveY = touchStartY - presentY;
+      // console.log('moveY',moveY)
+    }
+  }
+  spendSec.addEventListener('touchstart', touchStart);
+  spendSec.addEventListener('touchmove', touchMove);
+  document.addEventListener('touchend', () => {
+    if(targetCheck === true){
+      if(moveY > 0){
+        spendSec.classList.add('open');
+        // console.log('open','targetCheck',targetCheck)
+      }else{
+        spendSec.classList.remove('open');
+        // console.log('close','targetCheck',targetCheck)
+      }
+      targetCheck = false
     }
   })
-  
 })
 
